ca9fd567a66959cc95cc0f470afae6ba05c411c9
Julien Hnery <jwh_917@yahoo.com>
2022-08-24 07:59:51 -0400

    Using Attribute Accessor Macros

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,34 @@
 ## code your solution here. 
+# class Dog
+#   attr_reader :name
+#   attr_reader :breed
+
+#   # setter method
+#   def name=(name)
+#     @name = name
+#   end
+
+#   def breed=(breed)
+#     @breed = breed
+#   end
+
+# end
+
+class Cat
+  attr_accessor :name
+
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def meow
+    puts "meow!"
+  end
+
+
+end
